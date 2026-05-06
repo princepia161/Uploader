2cf64afab2275cf347d9f297198b94258333ad6b
princepia161 <42060271+princepia161@users.noreply.github.com>
2026-05-06 17:39:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,10 @@ async def restart_handler(_, m):
 @bot.on_message(filters.command(["txt"]))
 async def account_login(bot: Client, m: Message):
     editable = await m.reply_text("**Please Send TXT file for download**")
-    input: Message = await bot.listen(editable.chat.id)
+    input: Message = await bot.listen(
+    editable.chat.id,
+    filters=filters.document
+)
     y = await input.download()
     file_name, ext = os.path.splitext(os.path.basename(y))  # Extract filename & extension
     x = y  # No decryption, use the file as is
